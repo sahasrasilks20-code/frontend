2eadeec0a80590a55396e3724564c52b471b1387
Hari Kothapalli <harikothapalli314@gmail.com>
2026-05-26 21:59:13 +0530

    feat: transparent global image src resolver for production backend assets

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+// Automatically prepend production backend URL to relative image assets globally
+const originalSrcSetter = Object.getOwnPropertyDescriptor(HTMLImageElement.prototype, 'src').set;
+Object.defineProperty(HTMLImageElement.prototype, 'src', {
+  set(val) {
+    if (val && typeof val === 'string' && !val.startsWith('http') && !val.startsWith('data:') && !val.startsWith('blob:')) {
+      const backendUrl = import.meta.env.VITE_API_URL || '';
+      if (backendUrl) {
+        val = `${backendUrl.replace(/\/$/, '')}/${val.replace(/^\//, '')}`;
+      }
+    }
+    originalSrcSetter.call(this, val);
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
